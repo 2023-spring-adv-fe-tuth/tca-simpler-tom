4880c5036358f40a21bc279fb1f8aeb27da9308f
tsteelematc <tsteele@madisoncollege.edu>
2023-03-25 18:52:38 -0500

    Play and I won/lost button navigation...
    nav(-1) from Play to Home to keep browser history better ! ! !

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+
+    const nav = useNavigate();
+
     return (
-        <h1>
-            Home
-        </h1>
+        <>
+            <h1>
+                Home
+            </h1>
+            <Button 
+                type="primary" 
+                block
+                onClick={() => nav("/play")}
+            >
+                Play
+            </Button>        
+        </>
     );
 };
--- src/Play.js
+++ src/Play.js
@@ -1,7 +1,35 @@
+import { Button, Space } from "antd";
+import { useNavigate } from "react-router-dom";
+
 export const Play = () => {
+
+    const nav = useNavigate();
+
+    const gameOver = (won) => {
+        nav(-1);
+    };
+
     return (
-        <h1>
-            Play
-        </h1>
+        <>
+            <h1>
+                Play
+            </h1>
+            <Space>
+                <Button 
+                    type="primary" 
+                    block
+                    onClick={() => gameOver(true)}
+                >
+                    I Won
+                </Button>        
+                <Button 
+                    type="primary" 
+                    block
+                    onClick={() => gameOver(false)}
+                >
+                    I Lost
+                </Button>        
+            </Space>
+        </>
     );
 };
