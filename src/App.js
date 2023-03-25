90be37239da8a9591513637b833cbd06fa8b49ab
tsteelematc <tsteele@madisoncollege.edu>
2023-03-25 18:40:58 -0500

    HashRouter working...
    Tested with manually changing the Browser address bar...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { Home } from './Home';
 import { Play } from './Play';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Play />
+      <HashRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />} 
+          />
+          <Route
+            path='/play'
+            element={<Play />} 
+          />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
