b1c29b2f1a674b3588a9a6a4f4d90dfea29bf9c0
tsteelematc <tsteele@madisoncollege.edu>
2023-03-28 08:07:02 -0500

    Saving game results to the cloud...
    Brought in tca-cloud-api.js
    
    Installed it's dependency...
    npm install @aws-sdk/util-dynamodb
    
    Made call to saveGameToCloud() when adding a new game result to lifted state

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { Home } from './Home';
 import { Play } from './Play';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
+import { saveGameToCloud } from './tca-cloud-api';
 
 function App() {
 
   const [gameResults, setGameResults] = useState([]);
 
-  const addNewGameResult = (newGameResult) => setGameResults(
-    [
-      ...gameResults
+  const addNewGameResult = (newGameResult) => {
+    setGameResults(
+      [
+        ...gameResults
+        , newGameResult
+      ]
+    );
+
+    saveGameToCloud(
+      "tsteele@madisoncollege.edu"
+      , "tca-simpler-tom"
+      , new Date().toISOString()
       , newGameResult
-    ]
-  );
+    );
+  };
 
   return (
     <div className="App">
